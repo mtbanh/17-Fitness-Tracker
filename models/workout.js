55946ee2678624d94632a6ec81a3a5459058e921
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-06 20:41:35 -0700

    created html routes

--- models/workout.js
+++ models/workout.js
@@ -4,20 +4,20 @@ const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
     day: {type: Date, default:Date.now},
-    exercises: {
+    exercises: 
         [
             {
-            type: {type:String, trim:true},
-            name: {type:String, trim:true},
-            duration: {type:Number},
+            type: {type:String, trim:true, require: "Please enter an exercise type"},
+            name: {type:String, trim:true, require: "Please enter an exercise name"},
+            duration: {type:Number, require: "Please enter the exercise duration in minutes"},
             weight: {type:Number, default:0},
             reps: {type:Number, default:0},
             sets: {type:Number, default:0},
             distance: {type:Number, default:0}
             }
         ],
-        totalDuration: {type:Number, default:0}
-    }
+    totalDuration: {type:Number, default:0}
+
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,25 @@
 const db = require("../models");
+
+module.exports = (app)=>{
+   app.get("api/workouts", (req,res)=>{
+       db.Workout.find({})
+       .then(dbWorkout =>{
+           dbWorkout.forEach(workout=>{
+               let total =0;
+               workout.exercises.forEach(e=>{
+                   total+=e.duration;
+               })
+
+               workout.totalDuration = total
+           });
+
+           res.json(dbWorkout);
+       })
+       .catch(err=>{
+           console.log(err);
+           res.json(err);
+       })
+   });
+   
+   
+}
