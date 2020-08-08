27380a0b9a4433745f111660528df444f5fe3366
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-08-07 20:49:03 -0700

    created post api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,23 +3,43 @@ const db = require("../models");
 module.exports = (app)=>{
    app.get("api/workouts", (req,res)=>{
        db.Workout.find({})
-       .then(dbWorkout =>{
-           dbWorkout.forEach(workout=>{
-               let total =0;
-               workout.exercises.forEach(e=>{
-                   total+=e.duration;
-               })
-
-               workout.totalDuration = total
-           });
-
-           res.json(dbWorkout);
-       })
+        .populate("exercises")
+        .then(dbWorkout =>{
+            res.json(dbWorkout);
+        })
        .catch(err=>{
            console.log(err);
            res.json(err);
        })
    });
+   
+   app.get("/api/workouts/range", (req,res)=>{
+       db.Workout.find({})
+        .populate("exercises")
+        .then(dbWorkout =>{
+            res.json(dbWorkout)
+        })
+        .catch(err=>{
+            res.json(err)
+        })
+   })
+
+   app.post("/api/workouts/:id", (req,res)=>{
+       db.Workout.create(req.body)
+        .then(data=>{
+            db.Workout.findOneAndUpdate(
+                {_id: req.params.id},
+                {$push:{exercises:data._id}, $inc:{totalDuration:data.duration}},
+                {new:true}
+                .then(dbWorkout =>{
+                    res.json(dbWorkout)
+                })
+                .catch(err=>{
+                    res.json(err)
+                })
+            )
+        })
+   })
    
    
 }
